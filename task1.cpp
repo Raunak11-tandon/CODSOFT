dc27b4ce078544c13287c8e3df6b883dc29a1089
Raunak11-tandon <raunaktandon11@gmail.com>
2026-04-17 22:12:49 +0530

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>  // for rand() and srand()
+#include <ctime>    // for time()
+using namespace std;
+
+int main() {
+    int number, guess;
+    
+
+    // Seed random number generator
+    srand(time(0));
+
+    // Generate random number between 1 and 100
+    number = rand() % 100 + 1;
+
+    cout << "===== Number Guessing Game =====\n";
+    cout << "Guess a number between 1 and 100\n";
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess > number) {
+            cout << "Too high! Try again.\n";
+        } 
+        else if (guess < number) {
+            cout << "Too low! Try again.\n";
+        } 
+        else {
+            cout << "🎉 Correct! You guessed the number!\n";
+        }
+
+    } while (guess != number);
+
+    return 0;
+}
